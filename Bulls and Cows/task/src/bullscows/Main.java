a203b3f64aeea9eb66b6b8f8366587e5039e7837
katja <ekaterina.kozlova@canter.fi>
2023-02-16 17:50:17 +0200

    Stage 1/7: Game log

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -2,6 +2,17 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("""
+                The secret code is prepared: ****.
+                                
+                Turn 1. Answer:
+                1234
+                Grade: None.
+                               
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.""");
     }
 }
