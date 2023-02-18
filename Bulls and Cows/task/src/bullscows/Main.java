cbd4b70cb38c087deeda2b2412880831d39f3ca8
SunnyPanda <ekaterina.kozlova@canter.fi>
2023-02-18 23:14:21 +0200

    Stage 6/7: New level

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,42 +1,85 @@
 package bullscows;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static final Scanner in = new Scanner(System.in);
+    private static final Scanner IN = new Scanner(System.in);
+    private static final int LOWER_INT = 0;
+    private static final char LOWER_CHAR = '0';
+    private static final int MAX_NUMBER_OF_DIGITS = 10;
+    private static final char MIN_LETTER = 'a';
+    private static final int MAX_DIGIT = 9;
+
+
     public static void main(String[] args) {
-        System.out.println("Please, enter the secret code's length:");
         String secretCode = generateCode();
         System.out.println(secretCode);
         System.out.println("Okay, let's start a game!");
 
         int turn = 1;
         System.out.printf("Turn %d:\n", turn);
-        String guess = in.next();
+        String guess = IN.next();
         while (!grader(secretCode, guess)) {
             turn++;
             System.out.printf("Turn %d:\n", turn);
-            guess = in.next();
+            guess = IN.next();
         }
         System.out.println("Congratulations! You guessed the secret code.");
     }
 
     private static String generateCode() {
-        int digits = in.nextInt();
-        while (digits > 10) {
-            System.out.printf("Error: can't generate a secret number with a length of %s because there aren't enough " +
-                    "unique digits.\n", digits);
-            System.out.println("Please, enter the secret code's length:");
-            digits = in.nextInt();
+        int digits = askForCodeLength();
+        System.out.println("Input the number of possible symbols in the code:");
+        int possibleSymbols = IN.nextInt();
+
+        int diff = possibleSymbols - MAX_NUMBER_OF_DIGITS;
+        String code;
+        String pattern;
+        if (diff <= 0) {
+            pattern = String.format("%d-%d", LOWER_INT, possibleSymbols - 1);
+            code = generateNumberCode(digits, possibleSymbols);
+        } else {
+            pattern = String.format("%d-%d, %c-%c", LOWER_INT, MAX_DIGIT, MIN_LETTER, (char)(MIN_LETTER + diff - 1));
+            code = generateNumAndLetterCode(digits, diff);
         }
 
-        StringBuilder number = new StringBuilder();
-        while (number.length() < digits) {
-            int random = (int)(Math.random() * 10);
-            String strDigit = random == 0 && number.length() == 0 ? "" : "" + random;
-            if (!number.toString().contains(strDigit)) number.append(strDigit);
+        System.out.printf("The secret is prepared: %s (%s).\n", "*".repeat(digits), pattern);
+
+        return code;
+    }
+
+    private static String generateNumberCode(int digits, int possibleSymbols) {
+        Random random = new Random();
+        StringBuilder code = new StringBuilder();
+        while (code.length() < digits) {
+            char number = (char)(random.nextInt(possibleSymbols) + LOWER_CHAR);
+            if (code.indexOf(String.valueOf(number)) == -1) code.append(number);
         }
-        return number.toString();
+        return code.toString();
+    }
+
+    private static String generateNumAndLetterCode(int digits, int diff) {
+        Random random = new Random();
+        StringBuilder code = new StringBuilder();
+        int upper = LOWER_CHAR * 2 + diff;
+        while (code.length() < digits) {
+            char number = (char) (random.nextInt(upper - LOWER_CHAR) + LOWER_CHAR);
+            if (number <= MAX_DIGIT || number >= MIN_LETTER)
+                if (code.indexOf(String.valueOf(number)) == -1) code.append(number);
+        }
+        return code.toString();
+    }
+
+    private static int askForCodeLength() {
+        System.out.println("Input the length of the secret code:");
+        int digits = IN.nextInt();
+        while (digits > 36) {
+            System.out.println("Error. Please, input the length of a secret code that is less than 37.");
+            System.out.println("Input the length of the secret code:");
+            digits = IN.nextInt();
+        }
+        return digits;
     }
     private static boolean grader(String code, String guess) {
         int bulls = 0;
