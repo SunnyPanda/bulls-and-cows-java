163bd1738c4728454e9f91d951eeb5c12244212d
SunnyPanda <ekaterina.kozlova@canter.fi>
2023-02-19 13:38:52 +0200

    Stage 7/7: Error!

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -2,6 +2,7 @@ package bullscows;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final Scanner IN = new Scanner(System.in);
@@ -14,7 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
         String secretCode = generateCode();
-        System.out.println(secretCode);
+        if (secretCode == null) return;
+
         System.out.println("Okay, let's start a game!");
 
         int turn = 1;
@@ -29,9 +31,25 @@ public class Main {
     }
 
     private static String generateCode() {
-        int digits = askForCodeLength();
+        String digitsStr = IN.next();
+        int digits = isNumber(digitsStr) ? Integer.parseInt(digitsStr) : 0;
+        if (digits == 0) {
+            System.out.printf("Error: \"%s\" isn't a valid number.\n", digitsStr);
+            return null;
+        }
+
         System.out.println("Input the number of possible symbols in the code:");
         int possibleSymbols = IN.nextInt();
+
+        if (possibleSymbols > 36) {
+            System.out.println("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).");
+            return null;
+        }
+        if (digits > possibleSymbols) {
+            System.out.printf("Error: it's not possible to generate a code with a length of %d with %d unique symbols.\n",
+                    digits, possibleSymbols);
+            return null;
+        }
 
         int diff = possibleSymbols - MAX_NUMBER_OF_DIGITS;
         String code;
@@ -47,6 +65,11 @@ public class Main {
         System.out.printf("The secret is prepared: %s (%s).\n", "*".repeat(digits), pattern);
 
         return code;
+    }
+
+    private static boolean isNumber(String str) {
+        Pattern pattern = Pattern.compile("\\d+");
+        return pattern.matcher(str).matches();
     }
 
     private static String generateNumberCode(int digits, int possibleSymbols) {
@@ -71,16 +94,6 @@ public class Main {
         return code.toString();
     }
 
-    private static int askForCodeLength() {
-        System.out.println("Input the length of the secret code:");
-        int digits = IN.nextInt();
-        while (digits > 36) {
-            System.out.println("Error. Please, input the length of a secret code that is less than 37.");
-            System.out.println("Input the length of the secret code:");
-            digits = IN.nextInt();
-        }
-        return digits;
-    }
     private static boolean grader(String code, String guess) {
         int bulls = 0;
         int cows = 0;
